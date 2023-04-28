e327623450dfafbe92626f1ef2ec091b28ac3c0f
LAIEB Rachid <rachid.laieb@gmail.com>
2023-04-28 19:38:09 +0200

    ajout champ obligatoire

--- script.js
+++ script.js
@@ -3,7 +3,6 @@
 // localStorage. clear();
 
 
-
 const local = JSON.parse(localStorage.getItem("user"));
 
 if(local != null)
@@ -11,24 +10,29 @@ if(local != null)
     formulaire.style.display = "none";
     h1.textContent = `Bonjour ${local.nom}, tu as bien ${local.age} ans!`
 }
-
 else
 {
     bouton.onclick = () => {
+        // Add validation logic for mandatory fields
+        if (nom.value.trim() === "" || age.value.trim() === "") {
+            alert("Veuillez remplir tous les champs obligatoires.");
+            return;
+        }
+        
         const user = {
             nom: nom.value,
             age: age.value
         };
-        localStorage.setItem("user",JSON.stringify(user))
+        localStorage.setItem("user",JSON.stringify(user));
         document.location.reload();
     };
-
 }
 
-clear.onclick = () =>{
+clear.onclick = () => {
     localStorage.clear();
     document.location.reload();
 }
+
 
 
 
